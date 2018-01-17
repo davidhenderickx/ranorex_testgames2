7b92c3ad9b67289fc2700aeb5f3c134decf0e962
David Henderickx <david.henderickx@ctg.com>
2018-01-17 15:17:52 +0100

    Commit 1

---
+++ General/General/MyClass.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by Ranorex
+ * User: dhenderickx
+ * Date: 17/01/2018
+ * Time: 13:14
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+
+using Ranorex;
+
+namespace General
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		// TODO: Add Code to call testing functions - for example:	
+		// Automation.Start()
+	}
+}
